3d93caf89240638f3a5e94ec9f88a7fea0c42dca
anpe9592 <anpe9592@users.noreply.github.com>
2016-12-07 20:39:35 +0100

    Initial Commit

---
+++ mySnakeClone/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mySnakeClone
+//
+//  Created by Andreas Petersson on 2016-12-07.
+//  Copyright © 2016 Andreas Petersson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
