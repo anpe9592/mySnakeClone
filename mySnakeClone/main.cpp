8403f7a9f9713cfb85ab8bd48c98a26448485f90
anpe9592 <anpe9592@users.noreply.github.com>
2016-12-07 21:03:50 +0100

    add todo list

--- mySnakeClone/main.cpp
+++ mySnakeClone/main.cpp
@@ -1,11 +1,16 @@
-//
-//  main.cpp
-//  mySnakeClone
-//
-//  Created by Andreas Petersson on 2016-12-07.
-//  Copyright © 2016 Andreas Petersson. All rights reserved.
-//
+/*
+ TODO...
+ 
+ * create a window. size 640 480. black background.
+ * draw a white dot. SNAKE.
+ * make the dot move on it's own.
+ * add collision detection. stop snake dot move out of window
+ * draw dot at random place on the window. RAT.
+ * snake eat rat. rat disappear and draw new rat dot at random place
+ * snake grow by one dot after eating rat.
+ * game over when snake hits wall or it self
 
+*/
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
