5d16e2916248056b5ca287b6f61d8ccd9d008143
anpe9592 <anpe9592@users.noreply.github.com>
2016-12-18 17:18:25 +0100

    create the mouse class

---
+++ mySnakeClone/mouse.h
@@ -0,0 +1,29 @@
+
+#ifndef mouse_h
+#define mouse_h
+
+#include <iostream>
+
+#include <SDL2/SDL.h>
+
+class Mouse {
+public:
+    Mouse():
+    mPosX(0), mPosY(0), hit(false) {  }
+    
+    void setPos(int x, int y);
+    
+    void handleEvent(SDL_Event& event);     // Takes key input and adept the mouse velocity
+    void move(int width, int height);       // Move the mouse
+    void render(SDL_Renderer* renderer);    // Draw mouse on to screen
+    
+    static const int MOUSE_WIDTH  = 20;     // Mouse dot width
+    static const int MOUSE_HEIGHT = 20;     // Mouse dot height
+    
+private:
+    int mPosX, mPosY;   // The X and Y offset of the mouse dot
+    
+    bool hit;
+};
+
+#endif
