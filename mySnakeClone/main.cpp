85784add4641c4acf6ae7af40ca7dbfeb7d063f5
anpe9592 <anpe9592@users.noreply.github.com>
2017-01-19 08:55:19 +0100

    Revert "updated the todo list"
    This reverts commit 55cfd5bf099bba1817f6f878af2cd40100eb8420.

--- mySnakeClone/main.cpp
+++ mySnakeClone/main.cpp
@@ -3,15 +3,14 @@
  
  * [done]create a window. size 640 480. black background.
  * [done]draw a white dot. SNAKE.
- * [done]center the snake dot.
+ * [done]center the snake dot
  * [done]make the dot move on it's own.
- * [done]make snake move on user input.
- * [done]stop snake dot from moving out of the window.
+ * [done]make snake move on user input
+ * [done]add collision detection. stop snake dot move out of window
  * [done]draw dot at random place on the window. RAT.
- * add collision detection.
- * snake eat rat. rat disappear and draw new rat dot at random place.
+ * snake eat rat. rat disappear and draw new rat dot at random place
  * snake grow by one dot after eating rat.
- * game over when snake hits wall or it self.
+ * game over when snake hits wall or it self
 
 */
 #include "game.h"
