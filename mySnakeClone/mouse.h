c6634301ee5270a8e3ea36dd0432f4e5541fcf56
anpe9592 <anpe9592@users.noreply.github.com>
2017-01-19 08:58:08 +0100

    Revert "add collision code from pong game"
    This reverts commit e3bdb99f60584c8e0c2a7d752995b944eb9241a9.

--- mySnakeClone/mouse.h
+++ mySnakeClone/mouse.h
@@ -3,13 +3,13 @@
 #define mouse_h
 
 #include <iostream>
-#include <vector>
 
 #include <SDL2/SDL.h>
 
 class Mouse {
 public:
-    Mouse();
+    Mouse():
+    mPosX(0), mPosY(0), hit(false) {  }
     
     void setPos(int x, int y);
     
@@ -20,15 +20,8 @@ public:
     static const int MOUSE_WIDTH  = 20;     // Mouse dot width
     static const int MOUSE_HEIGHT = 20;     // Mouse dot height
     
-    std::vector<SDL_Rect>& getCollider();   // Gets the collision boxes
-    
-    bool collisionChecker(std::vector<SDL_Rect>& a, std::vector<SDL_Rect>& b);  // Checks collision
-    
 private:
     int mPosX, mPosY;   // The X and Y offset of the mouse dot
-    
-    std::vector<SDL_Rect> mColliders;       // The snake's collision boxes
-    void shiftColliders();                  // Moves the collision boxes relative to the snake's offset
     
     bool hit;
 };
