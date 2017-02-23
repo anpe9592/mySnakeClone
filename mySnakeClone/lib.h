7e5ccc51eb39236bcb1b2f585c34ceb02338d2c0
anpe9592 <anpe9592@users.noreply.github.com>
2017-02-23 12:48:26 +0100

    moved collisionDetector function to it own header file

---
+++ mySnakeClone/lib.h
@@ -0,0 +1,13 @@
+//
+//  lib.h
+//  mySnakeClone
+//
+//  Created by Andreas Petersson on 2017-02-23.
+//  Copyright © 2017 Andreas Petersson. All rights reserved.
+//
+
+#ifndef lib_h
+#define lib_h
+
+
+#endif /* lib_h */
