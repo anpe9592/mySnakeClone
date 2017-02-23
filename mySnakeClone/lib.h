d4562bab66cd831510b9de663536eb7546874beb
anpe9592 <anpe9592@users.noreply.github.com>
2017-02-23 12:50:23 +0100

    moved collisionDetector function to it own header file
    forgot to stage the files

--- mySnakeClone/lib.h
+++ mySnakeClone/lib.h
@@ -9,5 +9,41 @@
 #ifndef lib_h
 #define lib_h
 
+#include <SDL2/SDL.h>
+
+bool collisionDetector(SDL_Rect a, SDL_Rect b) {
+    int leftA,   leftB;
+    int rightA,  rightB;
+    int topA,    topB;
+    int bottomA, bottomB;
+    
+    leftA   = a.x;
+    rightA  = a.x + a.w;
+    topA    = a.y;
+    bottomA = a.y + a.h;
+    
+    leftB   = b.x;
+    rightB  = b.x + b.w;
+    topB    = b.y;
+    bottomB = b.y + b.h;
+    
+    if (bottomA <= topB) {
+        return false;
+    }
+    
+    if (topA >= bottomB) {
+        return false;
+    }
+    
+    if (rightA <= leftB) {
+        return false;
+    }
+    
+    if (leftA >= rightB) {
+        return false;
+    }
+    
+    return true;
+}
 
 #endif /* lib_h */
--- mySnakeClone/mouse.cpp
+++ mySnakeClone/mouse.cpp
@@ -1,41 +1,8 @@
 
 #include <random>
-#include "mouse.h"
+#include "lib.h"
 
-bool collisionDetector(SDL_Rect a, SDL_Rect b) {
-    int leftA,   leftB;
-    int rightA,  rightB;
-    int topA,    topB;
-    int bottomA, bottomB;
-    
-    leftA   = a.x;
-    rightA  = a.x + a.w;
-    topA    = a.y;
-    bottomA = a.y + a.h;
-    
-    leftB   = b.x;
-    rightB  = b.x + b.w;
-    topB    = b.y;
-    bottomB = b.y + b.h;
-    
-    if (bottomA <= topB) {
-        return false;
-    }
-    
-    if (topA >= bottomB) {
-        return false;
-    }
-    
-    if (rightA <= leftB) {
-        return false;
-    }
-    
-    if (leftA >= rightB) {
-        return false;
-    }
-    
-    return true;
-}
+#include "mouse.h"
 
 int mouseRandPos(int pos) {
     // Seed with a real random value, if available
