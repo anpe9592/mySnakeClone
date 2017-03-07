6115cb8b20717883c59a968871dc4b6c6ecc67e1
anpe9592 <anpe9592@users.noreply.github.com>
2017-03-07 16:26:48 +0100

    adding code to a Text class from an earlier project

--- mySnakeClone/text.cpp
+++ mySnakeClone/text.cpp
@@ -1,2 +1,54 @@
 
 #include "text.h"
+
+
+Text::Text() {
+    mText = NULL;
+    mTextWidth = 0;
+    mTextHeight = 0;
+}
+
+Text::~Text() {
+    deallocatesText();
+}
+
+bool Text::createImageFromString(SDL_Renderer* renderer, TTF_Font *font,std::string textureText, SDL_Color textColor) {
+    deallocatesText();
+    
+    SDL_Surface* textSurface = TTF_RenderText_Solid(font, textureText.c_str(), textColor);
+    if (textSurface != NULL) {
+        mText = SDL_CreateTextureFromSurface(renderer, textSurface);
+        if(mText != NULL) {
+            mTextWidth  = textSurface->w;
+            mTextHeight = textSurface->h;
+        } else {
+            std::cout << "Are not able to create texture from renderd text! SDL Error: " << SDL_GetError() << std::endl;
+        }
+        
+        SDL_FreeSurface(textSurface);
+    } else {
+        std::cout << "Can not render text surface! SDL_ttf Error: " << TTF_GetError() << std::endl;
+    }
+    
+    return mText != NULL;
+}
+
+void Text::deallocatesText() {
+    if (mText != NULL) {
+        SDL_DestroyTexture(mText);
+        mText = NULL;
+        mTextWidth = 0;
+        mTextHeight = 0;
+    }
+}
+
+void Text::renderText(SDL_Renderer* renderer, int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip) {
+    SDL_Rect renderQuad = { x, y, mTextWidth, mTextHeight };
+    
+    if(clip != NULL) {
+        renderQuad.w = clip->w;
+        renderQuad.h = clip->h;
+    }
+    
+    SDL_RenderCopyEx(renderer, mText, clip, &renderQuad, angle, center, flip);
+}
--- mySnakeClone/text.h
+++ mySnakeClone/text.h
@@ -3,5 +3,31 @@
 #define text_h
 
 #include <iostream>
+#include <string>
+
+#include <SDL2/SDL.h>
+#include <SDL2_ttf/SDL_ttf.h>
+
+class Text {
+public:
+    Text();
+    
+    ~Text();
+    
+    bool createImageFromString(SDL_Renderer* renderer, TTF_Font *font, std::string textureText, SDL_Color textColor);
+    
+    void deallocatesText();
+    
+    void renderText(SDL_Renderer* renderer, int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
+    
+    int getTextWidth()  const { return mTextWidth; }
+    int getTextHeight() const { return mTextHeight; }
+    
+private:
+    SDL_Texture* mText;
+    
+    int mTextWidth;
+    int mTextHeight;
+};
 
 #endif
